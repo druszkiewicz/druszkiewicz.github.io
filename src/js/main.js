b45386d026f538f2b98b5cf3353ce742697d02fa
Dariusz Ruszkiewicz <dariusz.ruszkiewicz@bader-leather.com>
2019-07-31 13:34:54 +0200

    fix: starter

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,20 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-   window.addEventListener('load', function () {
-      navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
-         // Registration was successful
-         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function (err) {
-         // registration failed :(
-         console.log('ServiceWorker registration failed: ', err);
-      });
-   });
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
 
 // place your code below
 
 
-console.log(`O...nice that you are here...`);
+console.log(`Hello world!`);
+
+
